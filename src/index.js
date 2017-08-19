8f157bba38d1f01a28beab7f735a529ee71b5235
guoshencheng <648772021@qq.com>
2017-08-19 17:11:01 +0800

    init function

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import program from 'commander';
 import packageJson from '../package.json';
 import chalk from 'chalk';
 
+import init from './scripts/init';
+
 program.version(packageJson.version)
-program.command('init').action(() => {
-  console.log(chalk.red('init'))
+program.command('init <name>').action((name) => {
+  init(name);
 })
 program.command('build').action(() => {
   console.log(chalk.red('build'))
