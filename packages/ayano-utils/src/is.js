a3fa3b412ff187c0adfa70d042819af17595a9e1
guoshencheng <648772021@qq.com>
2018-01-04 23:08:26 +0800

    add is

---
+++ packages/ayano-utils/src/is.js
@@ -0,0 +1,16 @@
+const ObjectString = obj =>  Object.prototype.toString.bind(obj)()
+
+export ObjectString;
+
+const _TYPES = ['Object', 'Array', 'String', 'Date', 'Number', 'Function', 'Boolean', 'Null', 'Undefined', 'Regexp'];
+
+export const TYPES = _TYPES.reduce((pre, cur) => {
+  pre[cur] = _TYPES[cur];
+  return pre;
+}, {})
+
+const is = (type) => (obj) => {
+  return ObjectString(obj) === `[object ${type}]`;
+}
+
+export default is;
