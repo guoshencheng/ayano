7f7afb87a180cabf6a779ea2cda20f64169a2b7a
guoshencheng <648772021@qq.com>
2018-01-06 14:49:11 +0800

    ayano-utils version 1.0.3

--- packages/ayano-utils/src/index.js
+++ packages/ayano-utils/src/index.js
@@ -1,9 +1,14 @@
-import is from './is';
-import version from './version';
-import brower from './brower';
+import is, { TYPES, ObjectString } from './is';
+import * as version from './version';
+import * as brower from './brower';
 import logger from './logger';
 
 exports.version = version;
+
+// is
 exports.is = is;
+exports.TYPES = TYPES;
+exports.ObjectString = ObjectString;
+
 exports.brower = brower;
 exports.logger = logger;
